47e7482fca16b54bf66a9bbfe3e7171d81384556
Rohitaswa Pradhan <rohitaswa96@gmail.com>
2021-05-28 12:54:14 +0530

    Assignment 3

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardImg, CardText, CardTitle } from 'reactstrap';
+import { Control, Errors, LocalForm } from 'react-redux-form';
+import { Button, Col, Label, Modal, ModalBody, ModalHeader, Row } from 'reactstrap';
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => (val) && (val.length >= len);
 
     function RenderDish({ dish }) {
         if (dish !== null) {
@@ -23,7 +29,7 @@ import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardImg, CardText, CardTitl
 
     function RenderComments({ comments }) {
         if (comments === null || comments.length === 0) {
-            return (<div></div>);
+            return (<div><CommentForm /></div>);
         }
         else {
             const commentList = comments.map((comment) => {
@@ -45,6 +51,7 @@ import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardImg, CardText, CardTitl
                     <ul class="list-unstyled">
                         {commentList}
                     </ul>
+                    <CommentForm />
                 </div>
             );
         }
@@ -77,6 +84,98 @@ import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardImg, CardText, CardTitl
         }
         else {
             return (<div></div>);
+        }
+    }
+
+    class CommentForm extends Component {
+        constructor(props) {
+            super(props);
+    
+            this.state = {
+                isModalOpen: false
+            };
+            this.toggleModal = this.toggleModal.bind(this);
+            this.handleSubmit = this.handleSubmit.bind(this);
+        }
+    
+        toggleModal() {
+            this.setState({
+                isModalOpen: !this.state.isModalOpen
+            });
+        }
+    
+        handleSubmit(values) {
+            console.log("Current state is " + JSON.stringify(values));
+            alert("Current state is " + JSON.stringify(values));
+        }
+    
+        render() {
+            return (
+                <React.Fragment>
+                    <Button outline color="secondary" onClick={this.toggleModal}>
+                        <span className="fa fa-pencil fa-lg"></span> Submit Comment
+                    </Button>
+                    <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                        <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                        <ModalBody>
+                            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                                <Row className="form-group">
+                                    <Col>
+                                        <Label htmlFor="rating">Rating</Label>
+                                        <Control.select model=".rating" name="rating" className="form-control">
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </Control.select>
+                                    </Col>
+                                </Row>
+                                <Row className="form-group">
+                                    <Col>
+                                        <Label htmlFor="author">Your Name</Label>
+                                        <Control.text model=".author" id=".author" name=".author"
+                                        className="form-control"
+                                        placeholder="Your Name"
+                                        validators={{
+                                            required,
+                                            minLength: minLength(3),
+                                            maxLength: maxLength(15)
+                                        }}
+                                        />
+                                        <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        messages={{
+                                            required: "Required",
+                                            minLength: "Must be greater than 2 characters",
+                                            maxLength: "Must be 15 characters or less"
+                                        }}
+                                        />
+                                    </Col>
+                                </Row>
+                                <Row className="form-group">
+                                    <Col>
+                                        <Label htmlFor="comment">Comment</Label>
+                                        <Control.textarea model=".comment" name="comment"
+                                        className="form-control"
+                                        rows="6"
+                                        />
+                                    </Col>
+                                </Row>
+                                <Row className="form-group">
+                                    <Col>
+                                        <Button type="submit" color="primary">
+                                            Submit
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </LocalForm>
+                        </ModalBody>
+                    </Modal>
+                </React.Fragment>
+            );
         }
     }
 
